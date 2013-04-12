1f9a3d9c2661757540ee5aaa81c24ca9ddb7b1b2
Adam Jiang <chaoj@nvidia.com>
2013-04-12 21:12:20 +0900

    Port to GTK 3

--- src/fbxkb.c
+++ src/fbxkb.c
@@ -298,7 +298,7 @@ get_group_info()
         ERR("can't alloc kbd info\n");
         exit(1);
     }
-    //kbd_desc_ptr->dpy = GDK_DISPLAY();
+    //kbd_desc_ptr->dpy = GDK_DISPLAY_XDISPLAY(gdk_display_get_default());
     if (XkbGetControls(dpy, XkbAllControlsMask, kbd_desc_ptr) !=
             Success) {
         ERR("can't get Xkb controls\n");
@@ -369,7 +369,8 @@ Xerror_handler(Display * d, XErrorEvent * ev)
     char buf[256];
 
     ENTER;
-    XGetErrorText(GDK_DISPLAY(), ev->error_code, buf, 256);
+    XGetErrorText(GDK_DISPLAY_XDISPLAY(gdk_display_get_default()),
+                  ev->error_code, buf, 256);
     ERR( "fbxkb : X error: %s\n", buf);
     RET();
 }
@@ -380,14 +381,15 @@ init()
     int dummy;
 
     ENTER;
-    if (!XkbQueryExtension(GDK_DISPLAY(), &dummy, &xkb_event_type, &dummy,
+    if (!XkbQueryExtension(GDK_DISPLAY_XDISPLAY(gdk_display_get_default()),
+                           &dummy, &xkb_event_type, &dummy,
             &dummy, &dummy)) {
         ERR("no XKB extension\n");
         exit(1);
     }
     XSetLocaleModifiers("");
     XSetErrorHandler((XErrorHandler) Xerror_handler);
-    dpy = GDK_DISPLAY();
+    dpy = GDK_DISPLAY_XDISPLAY(gdk_display_get_default());
     if (chdir(IMGPREFIX)) {
         ERR("can't chdir to %s\n", IMGPREFIX);
         exit(1);
@@ -420,7 +422,7 @@ main(int argc, char *argv[])
     GError *error = NULL;
 
     ENTER;
-    gtk_set_locale();
+    setlocale(LC_ALL, "");
     context = g_option_context_new("- X11 keyboard switcher");
     g_option_context_add_main_entries(context, entries, NULL);
     g_option_context_add_group(context, gtk_get_option_group(TRUE));
